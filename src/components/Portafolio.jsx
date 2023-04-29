3979c3f18457ef7c45d40076eda49bcf12e7b241
Arostegui Maranto <arosteguimaranto@outlook.com>
2023-04-28 20:36:44 -0600

    Portafolio item

--- src/components/Portafolio.jsx
+++ src/components/Portafolio.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import portafolio from "../data/portafolio"
+import { PortafolioItem } from "./PortafolioItem"
 
 export const Portafolio = () => {
   return (
-    <div>Portafolio</div>
+    <div className=" flex flex-col md:flex-row items-center justify-center">
+    <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+    {portafolio.map(project =>(
+      <PortafolioItem
+      imgUrl={project.imgUrl}
+      title={project.title}
+      stack={project.stack}
+      link={project.link}
+      />
+    ))}
+    </div>
+    </div>
   )
 }
---
+++ src/data/portafolio.js
@@ -0,0 +1,43 @@
+export default[
+    {
+        title: 'Hero App',
+        imgUrl: '',
+        stack: ['HTML', 'CSS', 'Javascript'],
+        link:'https://1hero-app.netlify.app',
+
+    },
+
+    {
+        title: 'Cotizador Crypto',
+        imgUrl: '',
+        stack: ['HTML', 'CSS', 'Javascript'],
+        imgUrl: '',
+        link:'https://1cripto-cotizador.netlify.app',
+
+    },
+
+    {
+        title: 'Control de Gastos',
+        imgUrl: '',
+        stack: ['HTML', 'CSS', 'Javascript'],
+        link:'https://1control-de-gastos.netlify.app',
+
+    },
+    {
+        title: 'Seguimiento de Pacientes ',
+        imgUrl: '',
+        stack: ['HTML', 'CSS', 'Javascript'],
+        link:'https://1seguimiento-de-pacientes-veterinaria.netlify.app',
+
+    },
+    {
+        title: 'Gifs App',
+        imgUrl: '',
+        stack: ['HTML', 'CSS', 'Javascript'],
+        link:'https://1gifsapp.netlify.app',
+
+    },
+    
+    
+
+]
