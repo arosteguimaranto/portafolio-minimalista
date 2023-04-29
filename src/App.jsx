42a756eb2d3dc0df4eb0035b0f99c81c3405b6a4
Arostegui Maranto <arosteguimaranto@outlook.com>
2023-04-28 18:46:35 -0600

    Intro

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 
+import { Contact } from './components/Contact'
+import { Footer } from './components/Footer'
+import { Intro } from './components/Intro'
+import { Portafolio } from './components/Portafolio'
+import { Timeline } from './components/Timeline'
 import './index.css'
 
 function App() {
 
 
   return (
-    <>
-      
-       
-    </>
+    <div>
+      <Intro />
+      <Timeline />
+      <Portafolio />
+      <Contact />
+      <Footer/>
+
+    </div>
   )
 }
 
