286549683316ddd006b8a9b66aec265551d7b1cf
Arostegui Maranto <arosteguimaranto@outlook.com>
2023-04-28 19:02:10 -0600

    Finalizando Intro

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+
 
 export const Intro = () => {
   return (
-    <div>Intro</div>
+    <div className='flex items-center justify-center  flex-col text-center pt-20 pb-6'>
+    <h1 className='text-4xl md:text-7xl mb-1 md:mb-3 font-bold'> Arostegui Maranto</h1>
+    <p className='text-base md:text-xl mb-3 font-medium'> Desarrollador de Frontend Junior</p>
+    <p className='text-sm max-w-xl mb-6 font-bold'> Apasionado por el diseño y la creación de soluciones atractivas y funcionales utilizando tecnologías web modernas.
+    <br/>
+    Si bien aún no tengo experiencia laboral en el campo, he adquirido habilidades prácticas a través de proyectos personales y cursos en línea.
+    </p>
+    </div>
   )
 }
