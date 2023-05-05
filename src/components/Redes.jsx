fb70b658bbd4dae54bf8751df35a4bf664efa4d9
Arostegui Maranto <arosteguimaranto@outlook.com>
2023-05-04 22:35:37 -0600

    Redes

--- src/components/Redes.jsx
+++ src/components/Redes.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import { FaGithub,  FaInstagram } from 'react-icons/fa';
 
 export const Redes = () => {
   return (
-    <div>Redes</div>
+    <div className="flex justify-center items-center space-x-4">
+    <a href="https://github.com/arosteguimaranto"><FaGithub size={50} /></a>
+    <a href="https://www.instagram.com/arosteguimaranto/"><FaInstagram size={50} /></a>
+  </div>
   )
 }
