13a1eff30db4dc88125b0e96ecf1cbaeee118ba7
Arostegui Maranto <arosteguimaranto@outlook.com>
2023-05-19 14:10:56 -0600

    arreglando detalles

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -4,7 +4,7 @@ export const Intro = () => {
   return (
     <div className='flex items-center justify-center  flex-col text-center pt-20 pb-6'>
     <h1 className='text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold'> Arostegui Maranto</h1>
-    <p className='text-base md:text-xl mb-3 font-medium'> Desarrollador de Frontend </p>
+    <p className='text-base md:text-xl mb-3 font-medium'> Desarrollador de Front-end </p>
     <p className='text-sm max-w-xl mb-6 font-bold'> Como desarrollador front-end, me apasiona crear experiencias web atractivas, funcionales y accesibles para los usuarios finales. He completado varios proyectos y cursos que me han permitido adquirir habilidades en el uso de tecnologías como HTML, CSS, JavaScript, React y Node.js.
     <br/>
     Además, tengo experiencia en el uso de herramientas de control de versiones como Git y Github para colaborar en proyectos en equipo y mantener un registro organizado de cambios y actualizaciones.
