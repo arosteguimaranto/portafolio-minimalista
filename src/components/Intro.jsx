12a9b0c4786bee5d0593185a30ef1315aebf8d51
Arostegui Maranto <arosteguimaranto@outlook.com>
2023-07-06 22:19:46 -0600

    Modificando intro

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -5,9 +5,9 @@ export const Intro = () => {
     <div className='flex items-center justify-center  flex-col text-center pt-20 pb-6'>
     <h1 className='text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold'> Arostegui Maranto</h1>
     <p className='text-base md:text-xl mb-3 font-medium'> Desarrollador Front-end </p>
-    <p className='text-sm max-w-xl mb-6 font-bold'>  Me apasiona crear experiencias web atractivas y funcionales He completado varios proyectos y cursos que me han permitido adquirir habilidades en el uso de tecnologías como HTML, CSS, JavaScript, React y Node.js.
+    <p className='text-sm max-w-xl mb-6 font-bold'>  Me apasiona crear experiencias web atractivas y funcionales, He completado varios proyectos y cursos que me han permitido adquirir habilidades en el uso de tecnologías como HTML, CSS, JavaScript, React y Node.js.
     <br/>
-    Además, tengo experiencia en el uso de herramientas de control de versiones como Git y Github.
+    Además, tengo experiencia con Git y Github e implementacion de testing con Jest.
     </p>
     <h3 className="text-sm max-w-xl mb-6 font-bold">  Comprometido con el aprendizaje continuo para mejorar y expandir mis habilidades.</h3>
     
