6fcbeb4ff8fc80954437d1063c660bc420ab92f8
arosteguimaranto <arosteguimaranto@outlook.com>
2024-02-28 02:59:20 -0600

    fix navbar

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,19 +3,23 @@
 export const Intro = () => {
   return (
     <div className='flex items-center justify-center flex-col  pt-20 pb-6'>
-    <h2 className='text-4xl text-center md:text-7xl dark:text-white mb-1 md:mb-3 tracking-tighter animate-pulse font-bold'> Arostegui Maranto</h2>
+    <h2 className='text-4xl text-center  md:text-7xl  dark:text-white mb-1 md:mb-3 tracking-tighter animate-pulse font-bold'> Arostegui Maranto</h2>
     <br/>
-    <p className=' tracking-tight md:text-3xl mb-3  font-mono'> <strong> Desarrollador Web </strong> </p>
-    
-    {/*<div class="justify-center w-full">
-    <ul class="h-8  items-center flex flex-wrap p-0 dark:bg-slate-100/80 bg-stone-950 text-white dark:text-black tracking-tighter font-mono hover:text-yellow-300 border rounded">
-        <li class="mx-5">Inicio</li>
+    <p className=' tracking-tight md:text-4xl mb-3  font-mono'> <strong> Desarrollador Web </strong> </p>
+    <br/>
+    <br/>
+    <div class="justify-center w-full">
+    <ul class="text-xl  flex flex-wrap justify-center p-3 dark:bg-slate-100/80 bg-stone-950 text-white dark:text-black tracking-tight font-semibold  border rounded-lg ">
+        <li class="mx-5"><a href=""> Inicio </a></li>
         <li class="mx-5">Proyectos</li>
         <li class="mx-5">Contacto</li>
-        <li class="mx-5">Redes Sociales</li>
+        
+        
     </ul>
 </div>
-  */}
+<br/>
+<br/>
+  
 
   <div class=" bg-stone p-8 rounded-lg  shadow-lg flex items-center">
         <div class="w-1/3">
@@ -25,11 +29,13 @@ export const Intro = () => {
             <h2 class=" tracking-tighter text-2xl font-bold mb-2">¿Quien soy?</h2>    
             <p class=" tracking-tighter text-lg font-mono dark:text-white"><strong>Hola,</strong> soy Dylan, un aprendiz autodidacta apasionado por desarrollo web y  la tecnologia, con habilidades prácticas cultivadas en proyectos y cursos en línea.</p>
         </div>
+        <br/>
     </div>
+    <br/>
     <div class=" w-full bg-stone p-5 m-5 rounded-lg shadow-lg flex items-center">
     <div class=" w-full">
-        <h2 class="tracking-tighter text-2xl   font-bold mb-5">Tecnologias </h2>
-        <ul class=" text-xl  flex flex-wrap justify-between dark:bg-slate-100/80 bg-stone-950 text-white dark:text-black tracking-tight font-semibold  hover:text-yellow-300  border rounded">
+        <h2 class="tracking-tight md:text-3xl mb-3 text-center font-mono "><strong>Tecnologias</strong> </h2>
+        <ul class=" text-xl  flex flex-wrap justify-between dark:bg-slate-100/80 bg-stone-950 text-white dark:text-black tracking-tight font-semibold  hover:text-yellow-300 transform hover:scale-110 duration-300 border rounded-lg  ">
         <li class="my-2 p-1 flex-none ">Html </li>
         <li class="my-2 p-1 flex-none">Css</li>
         <li class="my-2 p-1 flex-none">Javascript</li>
@@ -40,6 +46,7 @@ export const Intro = () => {
         <li class="my-2 p-1 flex-none">SQL </li>
         <li class="my-2 p-1 flex-none">Tailwind</li>  
         <li class="my-2 p-1 flex-none ">GitHub </li>
+        <li class="my-2 p-1 flex-none "> </li>
       </ul>
     </div>
 </div>
